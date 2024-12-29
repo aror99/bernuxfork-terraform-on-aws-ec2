72326e48d0a02eecf6fea119c6f30aa07a19b1b1
aror99 <aror99@gmail.com>
2024-12-29 13:20:22 -0600

    Update c6-outputs.tf

--- 05-Terraform-Loops-MetaArguments-SplatOperator/05-02-MetaArgument-for_each/terraform-manifests/c6-outputs.tf
+++ 05-Terraform-Loops-MetaArguments-SplatOperator/05-02-MetaArgument-for_each/terraform-manifests/c6-outputs.tf
@@ -6,6 +6,7 @@ output "instance_publicip" {
   description = "EC2 Instance Public IP"
   #value = aws_instance.myec2vm.*.public_ip   # Legacy Splat
   #value = aws_instance.myec2vm[*].public_ip  # Latest Splat
+  #Como usamos for-each, el splat operator no funciona, por lo que se tiene que hacer tambien el output por for-each
   value = toset([for instance in aws_instance.myec2vm: instance.public_ip])
 }
 
@@ -14,6 +15,7 @@ output "instance_publicdns" {
   description = "EC2 Instance Public DNS"
   #value = aws_instance.myec2vm[*].public_dns  # Legacy Splat
   #value = aws_instance.myec2vm[*].public_dns  # Latest Splat
+  #Como usamos for-each, el splat operator no funciona, por lo que se tiene que hacer tambien el output por for-each
   value = toset([for instance in aws_instance.myec2vm: instance.public_dns])
 }
 
