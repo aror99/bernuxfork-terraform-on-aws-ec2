a86afb45caf0a394c5b83042345ded349193b3f6
aror99 <aror99@gmail.com>
2024-12-30 20:35:09 -0600

    Update c2-v3-get-instancetype-supported-per-az-in-a-region.tf

--- 05-Terraform-Loops-MetaArguments-SplatOperator/05-03-Utility-Project/terraform-manifests/c2-v3-get-instancetype-supported-per-az-in-a-region.tf
+++ 05-Terraform-Loops-MetaArguments-SplatOperator/05-03-Utility-Project/terraform-manifests/c2-v3-get-instancetype-supported-per-az-in-a-region.tf
@@ -44,6 +44,7 @@ output "output_v3_2" {
 # Output-3
 # Filtered Output: with Keys Function - Which gets keys from a Map
 # This will return the list of availability zones supported for a instance type
+# Ahora si, esta es la solucion definitiva, nos regresa una lista de las regiones para las que si existe la instancia t3.micro
 output "output_v3_3" {
   value = keys({
     for az, details in data.aws_ec2_instance_type_offerings.my_ins_type: 
